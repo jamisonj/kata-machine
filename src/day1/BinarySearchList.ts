5e76e7b6a71fecf095aa2d977d8fc243ede89589
jamisonj <justin.jamison@outlook.com>
2024-12-03 16:56:26 -0700

    Binary Search.

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let lo = 0;
+    let hi = haystack.length;
+    do {
+        const midpoint = Math.floor(lo + (hi - lo) / 2);
+        const value = haystack[midpoint];
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            hi = midpoint;
+        } else {
+            lo = midpoint + 1;
+        }
+    } while (lo < hi);
+
+    return false;
+
 }
